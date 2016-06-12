0b06b5f4febec77313a9ad3a1782c5073889c644
“Krisgit config --global user.email “squeakyalien98@gmail.com <Krisbies@gmail.com>
2016-06-12 16:53:25 -0500

    setup constraints for routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+
+  def role_template
+    user_role = current_user.has_role? :admin ? 'mentor' : 'student'
+    "#{user_role}_#{action_name}"
+  end
 end
