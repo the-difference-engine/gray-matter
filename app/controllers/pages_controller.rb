197d5e0ce3c085590344d158d54df7549c3650ed
Austin Reichardt <areich6@Austins-MacBook-Pro.local>
2016-06-20 18:57:52 -0500

    added Rspec

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,17 @@
 class PagesController < ApplicationController
 
   def home 
-    
-    if current_user.role == "student" 
+    if !current_user 
+      redirect_to "/users/sign_in"
+    elsif current_user.role == "student" 
       render "student.html.erb"
     elsif current_user.role == "admin"
       @user = current_user
       render "admin.html.erb"
-    elsif current_user.role == "mentor"
+    else 
+      # current_user.role == "mentor"
       @user = current_user
       render "mentor.html.erb"
-    else 
-      redirect_to "/users/sign_in"
     end
   end
-
-
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   
   def show 
+  end
 
 
 end
