c7e4bc4a9cf92a61de840c3f52e6e6f964134b75
laurenjoyce <joycelc4186@gmail.com>
2016-07-18 20:57:23 -0500

    Updating nav bar to include links based on if clients are logged in

--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,18 @@ Rails.application.routes.draw do
     end
   end
 
-  
+
 #   get '/admins' => 'admins#index'
 #   post '/admins' => 'admins#create'
 #   get '/admins/:id' => 'admins#show'
 #   delete '/admins/:id' => 'admins#destroy'
 
-#   get '/mentors' => 'mentors#index'
+   get '/mentors' => 'mentors#index'
 #   post '/mentors' => 'mentors#create'
 #   get '/mentors/:id' => 'mentors#show'
 #   delete '/mentors/:id' => 'mentors#destroy'
 
-#   get '/students' => 'students#index'
+   get '/students' => 'students#index'
 #   post '/students' => 'students#create'
 #   get '/students/:id' => 'students#show'
 #   delete '/students/:id' => 'students#destroy'
