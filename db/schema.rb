4f6f301353f3cdc97345583bb64a8eec34ab4a95
Troy Leach <troyleach29@gmail.com>
2016-09-11 17:47:51 -0500

    added the model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160911203954) do
+ActiveRecord::Schema.define(version: 20160911224048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20160911203954) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "contact_email"
+  end
+
+  create_table "announcements", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "groups", force: :cascade do |t|
