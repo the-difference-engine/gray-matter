03fc9c87e095a7ed9da7e35c253b529eedce74b1
Troy Leach <troyleach29@gmail.com>
2016-09-13 08:18:19 -0500

    added the announcements

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
 
   def index
-    # TODO this is a total hack fix this
-    # redirect_to "/#{current_user.role}"
+    @header_styles = ["heading-color-one",
+                     "heading-color-two",
+                     "heading-color-three",
+                     "heading-color-four",
+                     "heading-color-five"]
     @home_url = authenticated_root_path
     @profile_url = "#{current_user.role}/#{current_user.id}" 
     @page_title = current_user.role.capitalize
+    @announcements = Announcement.all
+     
   end
 
 end
