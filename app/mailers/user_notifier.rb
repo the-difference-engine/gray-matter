4323b815955cfa31b8165a2d1c98cd763c80987a
Troy Leach <troyleach29@gmail.com>
2016-09-15 21:48:01 -0500

    added the env var

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,5 +1,5 @@
 class UserNotifier < ApplicationMailer
-  default :from => 'troyleach29@gmail.com'
+  default :from => 'graymatter411@gmail.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user, password)
