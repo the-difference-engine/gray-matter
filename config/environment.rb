4a0a30c1a4775be5a75bd85a7cb422f7e856c2c0
Troy Leach <troyleach29@gmail.com>
2016-10-01 13:50:50 -0500

    change the domain to domain

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
-  :domain => ENV["HOST_URL"],
+  :domain => ENV["DOMAIN"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
