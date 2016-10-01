07ad804b6ffc41e8c64b98542177c35bd40788d5
Troy Leach <troyleach29@gmail.com>
2016-10-01 12:19:05 -0500

    fixing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 User.create!(
-  {email: "admin@email.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, role: "admin", has_logged_in: false}
+  {email: "admin@email.com", password: "password", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, sign_in_count: 0, current_sign_in_at: nil, last_sign_in_at: nil, current_sign_in_ip: nil, last_sign_in_ip: nil, role: "admin", has_logged_in: false})
