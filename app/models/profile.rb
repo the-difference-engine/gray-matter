756642bbe68ce190d875091fd9e28c25c7338f15
Troy Leach <troyleach29@gmail.com>
2016-10-05 17:47:21 -0500

    I broke it

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -20,6 +20,6 @@
 class Profile < ActiveRecord::Base
   has_attached_file :avatar,
                     styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" },
-                    default_url: "https://simplerev.com/wp-content/uploads/Generic-Avatar.jpg",
+                    default_url: "https://simplerev.com/wp-content/uploads/Generic-Avatar.jpg"
                     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
