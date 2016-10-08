54dbaafb56f7aaab6a0470e80408305a37de4b4e
Troy Leach <troyleach29@gmail.com>
2016-10-08 15:42:38 -0500

    fixing up the env

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,10 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SENDGRID_USERNAME"],
   :password => ENV["SENDGRID_PASSWORD"],
-  :domain => 'demo-gray-matter.herokuapp.com',
+  :domain => ENV["DOMAIN"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
