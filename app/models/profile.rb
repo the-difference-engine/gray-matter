12108d187811cec060c05a70737b02f5b6e06690
Troy Leach <troyleach29@gmail.com>
2016-10-08 21:35:12 -0500

    added in validations for images and pdf

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -20,8 +20,6 @@
 class Profile < ActiveRecord::Base
   has_attached_file :avatar,
                     styles: { large: "600x600>", medium: "300x300>", thumb: "100x100#" }
-                    
-                    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-                    # validates_attachment :photo, :size => { :in => 0..4.megabytes }
-                    # validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
+                    validates_attachment :avatar, :size => { :in => 0..2.megabytes }
+                    validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
 end
