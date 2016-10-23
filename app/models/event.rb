24a5a0170650f3b5a74b6e4950319b659aa0c177
Troy Leach <troyleach29@gmail.com>
2016-10-23 18:35:24 -0500

    add all documents

--- app/models/event.rb
+++ app/models/event.rb
@@ -20,5 +20,5 @@
 
 class Event < ActiveRecord::Base
   has_attached_file :document
-  validates_attachment_content_type :document, :content_type => ['application/pdf']
+  validates_attachment_content_type :document, :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -15,5 +15,5 @@ class Resource < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :document
-  validates_attachment_content_type :document, :content_type => ['application/pdf']
+  validates_attachment_content_type :document, :content_type => %w(application/pdf application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document)
 end
