6d689e2b2862dd331c03b3bce41112c993eb53fd
Troy Leach <troyleach29@gmail.com>
2016-10-28 09:37:39 -0500

    fixing bug on admin resource document link

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -49,6 +49,5 @@ Rails.application.configure do
     :s3_credentials => {
       :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
       :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
-    }
   }
 end
