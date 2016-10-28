4f5c6aa68701db0bca507217a92d0d3faac49ddb
Troy Leach <troyleach29@gmail.com>
2016-10-28 01:30:52 -0500

    trying to fix aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,6 +85,7 @@ Rails.application.configure do
     :bucket => "graymatterapplication",
     :s3_host_name => "s3-us-west-2.amazonaws.com",
     :default_url => "https://s3-us-west-2.amazonaws.com/graymatterapplication/default/generic-avatar.jpg",
+    :s3_region => "us-west-2",
     :s3_credentials => {
       :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
       :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]
