0be8985388a835b9f2cc6c2c13dcbc839619d170
Troy Leach <troyleach29@gmail.com>
2016-11-20 16:28:02 -0600

    moved the js to a file

---
+++ app/assets/javascripts/mainJs.js
@@ -0,0 +1,23 @@
+function addFields() {
+    var number = document.getElementById("member").value;
+    var container = document.getElementById("container");
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+    }
+    for(i=0; i < number; i++){
+        container.appendChild(document.createTextNode("link " + (i+1)));
+        var input = document.createElement("input");
+        input.id = "link";
+        input.type = "text";
+        input.name = "links[]";
+        input.style = "width: 390px";
+        container.appendChild(input);
+        container.appendChild(document.createElement("br"));
+    }
+}
+
+function removeFields(e) {
+    var parent = document.getElementById("link-container");
+    var link_input = document.getElementById(e);
+    parent.removeChild(link_input);
+}
